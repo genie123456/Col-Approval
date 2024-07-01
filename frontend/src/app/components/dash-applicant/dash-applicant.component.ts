d553af704e440ac4b244bce4b172964524456046
genie123456 <pv14720@gmail.com>
2024-07-01 19:42:24 +0530

    updated 1 July

--- frontend/src/app/components/dash-applicant/dash-applicant.component.ts
+++ frontend/src/app/components/dash-applicant/dash-applicant.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./dash-applicant.component.css']
 })
 export class DashApplicantComponent {
-
 }
--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,55 +1,17 @@
-import { Component, AfterViewInit, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-
-declare var bootstrap: any;
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements AfterViewInit, OnInit {
+export class SidebarComponent implements OnInit {
   loggedIn: boolean = false;
   username: string = '';
 
-  constructor(private authService: AuthService, private cd: ChangeDetectorRef) { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.authService.isLoggedIn().subscribe((status: boolean) => {
-      this.loggedIn = status;
-      if (status) {
-        const user = JSON.parse(localStorage.getItem('user') || '{}');
-        this.username = user.username || '';
-        this.cd.detectChanges();  // Trigger change detection
-      }
-    });
-
-    this.authService.user$.subscribe((user) => {
-      if (user) {
-        this.username = user.username;
-        this.cd.detectChanges();  // Trigger change detection
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    const hamBurger: HTMLElement | null = document.querySelector(".toggle-btn");
-
-    if (hamBurger) {
-      hamBurger.addEventListener("click", () => {
-        const sidebar: HTMLElement | null = document.querySelector("#sidebar");
-        if (sidebar) {
-          sidebar.classList.toggle("expand");
-        }
-      });
-    }
-
-    // Manually initialize Bootstrap collapse
-    const collapseElements = document.querySelectorAll('.collapse');
-    collapseElements.forEach(collapseElement => {
-      new bootstrap.Collapse(collapseElement, {
-        toggle: false
-      });
-    });
   }
 }
